79814e049a1ac3efd9e4db405162604b7cce7e45
Marcin Król <marcins1024@gmail.com>
2017-06-19 16:34:10 +0200

    Another hello

--- src/sages/bootcamp/git/GitHello.java
+++ src/sages/bootcamp/git/GitHello.java
@@ -4,5 +4,6 @@ public class GitHello {
   public static void main(String[] args) {
     System.out.println("Hello git!");
     System.out.println("Feature branch hello!");
+    System.out.println("Feature branch another hello!");
   }
 }
