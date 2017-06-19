1102e73d9ede8d2269f725fdbe8c4be9ed54f5fb
Marcin Król <marcins1024@gmail.com>
2017-06-19 16:31:30 +0200

    Feature branch hello

--- src/sages/bootcamp/git/GitHello.java
+++ src/sages/bootcamp/git/GitHello.java
@@ -3,5 +3,6 @@ package sages.bootcamp.git;
 public class GitHello {
   public static void main(String[] args) {
     System.out.println("Hello git!");
+    System.out.println("Feature branch hello!");
   }
 }
